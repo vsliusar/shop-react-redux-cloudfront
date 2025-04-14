9321b2f16f89d607eac7085145dc1727b6d3a8ba
Vitalii Sliusar <vitalii_sliusar@epam.com>
2025-04-14 22:23:55 +0200

    task-2 completed

---
+++ infra/lib/deployment-service.ts
@@ -0,0 +1,66 @@
+import {
+  aws_cloudfront,
+  aws_cloudfront_origins,
+  aws_s3,
+  aws_s3_deployment,
+  CfnOutput,
+  RemovalPolicy,
+} from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+const path = "../dist";
+
+export class DeploymentService extends Construct {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    const hostingBucket = new aws_s3.Bucket(this, "FrontendBucket2", {
+      publicReadAccess: false,
+      blockPublicAccess: aws_s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    const distribution = new aws_cloudfront.Distribution(
+      this,
+      "CloudfrontDistribution",
+      {
+        defaultBehavior: {
+          origin:
+            aws_cloudfront_origins.S3BucketOrigin.withOriginAccessControl(
+              hostingBucket
+            ),
+          viewerProtocolPolicy:
+            aws_cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        },
+        defaultRootObject: "index.html",
+        errorResponses: [
+          {
+            httpStatus: 404,
+            responseHttpStatus: 200,
+            responsePagePath: "/index.html",
+          },
+        ],
+      }
+    );
+
+    new aws_s3_deployment.BucketDeployment(this, "BucketDeployment2", {
+      sources: [aws_s3_deployment.Source.asset(path)],
+      destinationBucket: hostingBucket,
+      distribution,
+      distributionPaths: ["/*"],
+    });
+
+    new CfnOutput(this, "CloudFrontURL", {
+      value: distribution.domainName,
+      description: "The distribution URL",
+      exportName: "CloudfrontURL-v2",
+    });
+
+    new CfnOutput(this, "BucketName", {
+      value: hostingBucket.bucketName,
+      description: "The name of the S3 bucket",
+      exportName: "BucketName-v2",
+    });
+  }
+}
