e88a2cec16349eefa0be092ab4b90b90e5368498
Vitalii Sliusar <vitalii_sliusar@epam.com>
2025-06-20 12:10:25 +0200

    task-8

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://jxux1yhpi8.execute-api.us-east-1.amazonaws.com/prod/",
   import: "https://k5kpdl33ul.execute-api.us-east-1.amazonaws.com/prod",
   bff: "https://jxux1yhpi8.execute-api.us-east-1.amazonaws.com/prod/",
-  cart: "https://jxux1yhpi8.execute-api.us-east-1.amazonaws.com/prod/",
+  cart: "https://ru5i4yw3d9.execute-api.us-east-1.amazonaws.com/prod/api",
 };
 
 export default API_PATHS;
